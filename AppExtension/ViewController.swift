8a3b857534bb7bba186d4dd3778ea3dd7e92d9d1
Joey Zhang <joey.zhang@tgitech.net>
2020-12-29 18:09:35 +0800

    feat: empty project

---
+++ AppExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppExtension
+//
+//  Created by joey on 12/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("all good")
+    }
+
+
+}
+
